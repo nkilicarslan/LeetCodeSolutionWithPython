a089a4e2e155fab9f1a0e7892a9218a9b79060ab
Necat Kılıçarslan <necatkilicarslann@gmail.com>
2023-04-26 19:00:06 +0300

    add problem 1816

--- Truncate Sentence.py
+++ Truncate Sentence.py
@@ -1,3 +1,5 @@
 class Solution:
+    # this function will return the truncated sentence
     def truncateSentence(self, s: str, k: int) -> str:
+        # split the string into a list of words and return the first k words with joining them with a space
         return ''.join(s.split()[:k])

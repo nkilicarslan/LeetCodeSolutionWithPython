5755e07d072073eea1353fdc11cf7e23187133de
Necat Kılıçarslan <necatkilicarslann@gmail.com>
2023-04-26 18:54:41 +0300

    add problem 409

--- Longest Palindrome.py
+++ Longest Palindrome.py
@@ -1,24 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         letter_dp = {}
+        add_one = False
         count = 0
-        is_one = False
         for char in s:
             if char not in letter_dp:
                 letter_dp[char] = 1
             else:
                 letter_dp[char] += 1
-        values_of_letter_dp = list(letter_dp.values())
-        for value in values_of_letter_dp:
-            if value % 2 == 0:
-                count += value
-            else:
-                count += value - 1
-                if count == 1:
-                    is_one = True
-        if 1 in values_of_letter_dp or is_one:
-            return count + 1
+        if len(letter_dp.items()) == 1:
+            return len(s)
         else:
-            return count
-a = Solution()
-a.longestPalindrome("abccccdd")
+            for value in letter_dp.values():
+                if value % 2 == 0:
+                    count += value
+                else:
+                    count += value - 1
+                    add_one = True
+
+        if add_one == True:
+            return count + 1
+        return count

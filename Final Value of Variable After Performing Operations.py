9884d4333040d43c6365c49846c7ffb243988a89
Necat Kılıçarslan <necatkilicarslann@gmail.com>
2023-05-06 00:56:41 +0300

    add problem 2011

--- Final Value of Variable After Performing Operations.py
+++ Final Value of Variable After Performing Operations.py
@@ -1,9 +1,15 @@
 class Solution:
+    # this function will return the final value of the variable after performing the operations
     def finalValueAfterOperations(self, operations: List[str]) -> int:
+        # create a variable to store the final result
         final_result = 0
+        # iterate over the operations list
         for operation in operations:
+            # if the operation is '--X' or 'X--', decrement the final_result by 1
             if operation[0] == '-' or operation[-1] == '-':
                 final_result -= 1
+            # otherwise if the operation is '++X' or 'X++', increment the final_result by 1
             else:
                 final_result += 1
+        # return the final_result
         return final_result

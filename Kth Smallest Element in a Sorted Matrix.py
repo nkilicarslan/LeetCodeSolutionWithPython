b90c1535b12e337234b5e870f51701f725a0d5b5
Necat Kılıçarslan <necatkilicarslann@gmail.com>
2023-05-11 20:28:24 +0300

    add problem 378

--- Kth Smallest Element in a Sorted Matrix.py
+++ Kth Smallest Element in a Sorted Matrix.py
@@ -1,8 +1,14 @@
 class Solution:
+    # this function will return the kth smallest element in the matrix
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        # create a list to store all the values of the matrix
         all_values_of_matrix = []
+        # iterate over the matrix and add the values to the list
         for list in matrix:
             for value in list:
+                # add the value to the list
                 all_values_of_matrix.append(value)
+        # sort the list
         all_values_of_matrix = sorted(all_values_of_matrix)
+        # return the kth smallest element
         return all_values_of_matrix[k-1]
